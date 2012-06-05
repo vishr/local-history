d2e583e48392bd543542022cf79a8005c5244ead
tollus <andy.solodyna@gmail.com>
2012-06-05 15:34:35 -0300

    modified get_filedir to work with network paths in windows too

--- history.py
+++ history.py
@@ -40,7 +40,10 @@ def show_diff(window, diff):
 def get_filedir(file_path):
     file_dir = os.path.dirname(file_path)
     if platform.system() == "Windows":
-        file_dir = file_dir.replace(":", "", 1)
+        if file_dir.find("\\\\") == 0:
+            file_dir = file_dir[2:] # strip the network \\ starting path
+        if file_dir.find(":") == 1:
+            file_dir = file_dir.replace(":", "", 1)
     else:
         file_dir = file_dir[file_dir.find(os.sep) + 1:]  # Trim the root
     return os.path.join(history_path, file_dir)
