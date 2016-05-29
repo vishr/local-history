d7ebaaacc7d8e51e8e4fc8ff6df80def15f50f49
jrappen <jrappen@users.noreply.github.com>
2016-05-29 19:15:04 +0200

    Hotfix all issues and implement pull request features (#62)
    Fixed #22, fixed #30, fixed #39, fixed #40, fixed #50, fixed #52, fixed #53, fixed #55, fixed #56, fixed #58, fixed #59.
    
    * Rename Context.sublime-menu to menus/Context.sublime-menu
    
    * Update Context.sublime-menu
    
    * Rename Main.sublime-menu to menus/Main.sublime-menu
    
    * Update Main.sublime-menu
    
    * Rename Default.sublime-commands to commands/Default.sublime-commands
    
    * Update Default.sublime-commands
    
    * Update and rename LocalHistory.sublime-settings to settings/Local History.sublime-settings
    
    * Update package-metadata.json
    
    * Update and rename history.py to Local History.py
    
    * Delete package-metadata.json
    
    * Update README.md
    
    * Update Main.sublime-menu
    
    * Add files via upload
    
    * Create context-menu.png
    
    * Delete context-menu.png
    
    * Delete tools-menu.png
    
    * Create .gitkeep
    
    * Add files via upload
    
    * Delete .gitkeep
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update Local History.sublime-settings
    
    * Update Local History.sublime-settings
    
    * Update README.md
    
    * Update and rename Local History.py to LocalHistory.py
    
    * Rename Local History.sublime-settings to LocalHistory.sublime-settings
    
    * fix path target for read-only settings
    
    * Update Main.sublime-menu
    
    * add seperator for visuals to menu

---
+++ LocalHistory.py
@@ -0,0 +1,309 @@
+import sys
+import os
+import platform
+import datetime
+import difflib
+import filecmp
+import shutil
+from threading import Thread
+import subprocess
+import sublime
+import sublime_plugin
+
+PY2 = sys.version_info < (3, 0)
+NO_SELECTION = -1
+
+def status_msg(msg):
+    sublime.status_message('Local History: ' + msg)
+
+def get_setting(requested_setting):
+    settings = sublime.load_settings('LocalHistory.sublime-settings')
+
+    if requested_setting == 'history_retention':
+        return settings.get('history_retention', 730)
+    elif requested_setting == 'format_timestamp':
+        return settings.get('format_timestamp', '%Y%m%d%H%M%S')
+    elif requested_setting == 'history_on_close':
+        return settings.get('history_on_close', True)
+    elif requested_setting == 'history_on_focus_lost':
+        return settings.get('history_on_focus_lost', False)
+    elif requested_setting == 'history_on_load':
+        return settings.get('history_on_load', True)
+    elif requested_setting == 'portable':
+        return settings.get('portable', True)
+    elif requested_setting == 'file_size_limit':
+        return settings.get('file_size_limit', 4194304)
+
+def get_history_root():
+    return os.path.join(os.path.dirname(sublime.packages_path()), '.sublime', 'Local History') if get_setting('portable') else os.path.join(os.path.abspath(os.path.expanduser('~')), '.sublime', 'Local History')
+
+def get_history_subdir(file_path):
+    history_root = get_history_root()
+
+    file_dir = os.path.dirname(file_path)
+    if platform.system() == 'Windows':
+        if file_dir.find(os.sep) == 0:
+            file_dir = file_dir[2:]
+        if file_dir.find(':') == 1:
+            file_dir = file_dir.replace(':', '', 1)
+    else:
+        file_dir = file_dir[1:]
+
+    return os.path.join(history_root, file_dir)
+
+def get_history_files(file_name, history_dir):
+    file_root, file_extension = os.path.splitext(file_name)
+    history_files = [os.path.join(dirpath, f)
+                        for dirpath, dirnames, files in os.walk(history_dir)
+                        for f in files if f.startswith(file_root) and f.endswith(file_extension)]
+    history_files.sort(key=lambda f: os.path.getmtime(os.path.join(history_dir, f)), reverse=True)
+
+    return history_files
+
+class OpenLocalHistoryDefaultSettingsCommand(sublime_plugin.WindowCommand):
+    def __init__(self, *args, **kwargs):
+        super(OpenLocalHistoryDefaultSettingsCommand, self).__init__(*args, **kwargs)
+        self.view = None
+
+    def run(self):
+        self.view = sublime.active_window().open_file(
+            os.path.join(
+                sublime.installed_packages_path(),
+                'Local History',
+                'settings',
+                'LocalHistory.sublime-settings'
+            )
+        )
+        sublime.set_timeout(self.set_view_readonly, 1)
+
+    def set_view_readonly(self):
+        if self.view is None or self.view.is_loading():
+            sublime.set_timeout(self.set_view_readonly, 1)
+            return
+        self.view.set_read_only(True)
+
+class HistorySave(sublime_plugin.EventListener):
+
+    def on_load(self, view):
+        if not PY2:
+            return
+
+        if get_setting('history_on_load'):
+            t = Thread(target=self.process_history, args=(view.file_name(),))
+            t.start()
+
+    def on_load_async(self, view):
+        if get_setting('history_on_load'):
+            t = Thread(target=self.process_history, args=(view.file_name(),))
+            t.start()
+
+    def on_close(self, view):
+        if get_setting('history_on_close'):
+            t = Thread(target=self.process_history, args=(view.file_name(),))
+            t.start()
+
+    def on_post_save(self, view):
+        if not PY2:
+            return
+
+        if not get_setting('history_on_close'):
+            t = Thread(target=self.process_history, args=(view.file_name(),))
+            t.start()
+
+    def on_post_save_async(self, view):
+        if not get_setting('history_on_close'):
+            t = Thread(target=self.process_history, args=(view.file_name(),))
+            t.start()
+
+    def on_deactivated(self, view):
+        if (view.is_dirty() and get_setting('history_on_focus_lost')):
+            t = Thread(target=self.process_history, args=(view.file_name(),))
+            t.start()
+
+    def process_history(self, file_path):
+        if file_path == None:
+            status_msg('File path does not exist.')
+            return
+
+        size_limit = get_setting('file_size_limit')
+        history_retention = get_setting('history_retention')
+
+        if PY2:
+            file_path = file_path.encode('utf-8')
+        if os.path.getsize(file_path) > size_limit:
+            status_msg('File not saved, exceeded {0} KB limit.'.format(size_limit / 1024))
+            return
+
+        file_name = os.path.basename(file_path)
+        history_dir = get_history_subdir(file_path)
+        if not os.path.exists(history_dir):
+            os.makedirs(history_dir)
+
+        history_files = get_history_files(file_name, history_dir)
+
+        if history_files:
+            if filecmp.cmp(file_path, os.path.join(history_dir, history_files[0])):
+                status_msg('No changes, skipped updating history for "' + file_name + '".')
+                return
+
+        file_root, file_extension = os.path.splitext(file_name)
+        shutil.copyfile(file_path, os.path.join(history_dir, '{0}-{1}{2}'.format(file_root, datetime.datetime.now().strftime(get_setting('format_timestamp')), file_extension)))
+
+        status_msg('Updated Local History for "' + file_name + '".')
+
+        if history_retention == 0:
+            return
+
+        max_valid_archive_date = datetime.date.today() - datetime.timedelta(days=history_retention)
+        for file in history_files:
+            file = os.path.join(history_dir, file)
+            if datetime.date.fromtimestamp(os.path.getmtime(file)) < max_valid_archive_date:
+                os.remove(file)
+
+class HistoryBrowse(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        target_dir = get_history_subdir(self.view.file_name())
+        system = platform.system()
+        if system == 'Darwin':
+            subprocess.call(['open', target_dir])
+        elif system == 'Linux':
+            subprocess.call(['xdg-open', target_dir])
+        elif system == 'Windows':
+            subprocess.call(['explorer', target_dir])
+
+class HistoryOpen(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        file_name = os.path.basename(self.view.file_name())
+        history_dir = get_history_subdir(self.view.file_name())
+
+        history_files = get_history_files(file_name, history_dir)
+        if not history_files:
+            status_msg('Local History not found for "' + file_name + '".')
+            return
+
+        def on_done(index):
+            if index is NO_SELECTION:
+                return
+
+            self.view.window().open_file(os.path.join(history_dir, history_files[index]))
+
+        self.view.window().show_quick_panel(history_files, on_done)
+
+class HistoryCompare(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        file_name = os.path.basename(self.view.file_name())
+        history_dir = get_history_subdir(self.view.file_name())
+
+        history_files = get_history_files(file_name, history_dir)
+        history_files = history_files[1:]
+
+        if not history_files:
+            status_msg('Local History not found for "' + file_name + '".')
+            return
+
+        def on_done(index):
+            if index is NO_SELECTION:
+                return
+
+            if self.view.is_dirty():
+                self.view.run_command('save')
+
+            from_file = os.path.join(history_dir, history_files[index])
+            to_file = self.view.file_name()
+            self.view.run_command('show_diff', {'from_file': from_file, 'to_file': to_file})
+
+        self.view.window().show_quick_panel(history_files, on_done)
+
+class HistoryReplace(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        file_name = os.path.basename(self.view.file_name())
+        history_dir = get_history_subdir(self.view.file_name())
+
+        history_files = get_history_files(file_name, history_dir)
+        history_files = history_files[1:]
+
+        if not history_files:
+            status_msg('Local History not found for "' + file_name + '".')
+            return
+
+        def on_done(index):
+            if index is NO_SELECTION:
+                return
+
+            file = os.path.join(history_dir, history_files[index])
+            with open(file) as f:
+                data = f.read()
+                if PY2:
+                    data.decode('utf-8')
+                self.view.replace(edit, sublime.Region(0, self.view.size()), data)
+            self.view.run_command('save')
+
+        self.view.window().show_quick_panel(history_files, on_done)
+
+class HistoryIncrementalDiff(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        file_name = os.path.basename(self.view.file_name())
+        history_dir = get_history_subdir(self.view.file_name())
+
+        history_files = get_history_files(file_name, history_dir)
+        if len(history_files) < 2:
+            status_msg('Incremental diff not found for "' + file_name + '".')
+            return
+
+        def on_done(index):
+            if index is NO_SELECTION:
+                return
+
+            if index == len(history_files) - 1:
+                status_msg('Incremental diff not found for "' + file_name + '".')
+                return
+
+            from_file = os.path.join(history_dir, history_files[index + 1])
+            to_file = os.path.join(history_dir, history_files[index])
+            self.view.run_command('show_diff', {'from_file': from_file, 'to_file': to_file})
+
+        self.view.window().show_quick_panel(history_files, on_done)
+
+class ShowDiff(sublime_plugin.TextCommand):
+
+    def run(self, edit, **kwargs):
+        from_file = kwargs['from_file']
+        to_file = kwargs['to_file']
+        if PY2:
+            from_file = from_file.encode('utf-8')
+            with open(from_file, 'r') as f:
+                from_content = f.readlines()
+        else:
+            with open(from_file, 'r', encoding='utf-8') as f:
+                from_content = f.readlines()
+
+        if PY2:
+            to_file = to_file.encode('utf-8')
+            with open(to_file, 'r') as f:
+                to_content = f.readlines()
+        else:
+            with open(to_file, 'r', encoding='utf-8') as f:
+                to_content = f.readlines()
+
+        diff = difflib.unified_diff(from_content, to_content, from_file, to_file)
+        diff = ''.join(diff)
+        if PY2:
+            diff = diff.decode('utf-8')
+        panel = sublime.active_window().new_file()
+        panel.set_scratch(True)
+        panel.set_syntax_file('Packages/Diff/Diff.sublime-syntax')
+        panel.insert(edit, 0, diff)
+
+class HistoryDeleteAll(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        if not sublime.ok_cancel_dialog('Are you sure you want to delete the Local History for all files?'):
+            return
+
+        shutil.rmtree(get_history_root())
+        status_msg('The Local History has been deleted for all files.')
--- README.md
+++ README.md
@@ -1,45 +1,65 @@
 # Local History
-A Sublime Text 2/3 plugin for maintaining local history of files
+
+[![GitHub license](https://img.shields.io/github/license/vishr/local-history.svg?style=flat-square)](https://github.com/vishr/local-history/tree/master/LICENSE.md)
+[![Total downloads via Package Control](https://img.shields.io/packagecontrol/dt/Local%20History.svg?style=flat-square)](https://packagecontrol.io/packages/Local%20History)
+[![Monthly downloads via Package Control](https://img.shields.io/packagecontrol/dm/Local%20History.svg?style=flat-square)](https://packagecontrol.io/packages/Local%20History)
+
+
+A [Sublime Text](https://www.sublimetext.com) package for maintaining a local history of files.
 
 ## Benefits
 
-* Every time you modify a file, a copy of the old contents is kept in the local history.
-* At any time, you can compare a file with any older version from the history.
-* View incremental diff of any file from the history.
-* It can help you out when you change or delete a file by accident.
-* The history can also help you out when your workspace has a catastrophic problem or if you get disk errors that corrupt your workspace files.
-* Each file revision is stored in a separate file (with full path) inside the `.sublime/history` folder of your home directory.
-e.g., `/Users/vrana/.sublime/history/foo/bar/Oct.14.2012_14.48.47.history.py`
+* Every time you modify a file, a copy of the old contents is kept in the local history when you:
+  * open the file.
+  * close the file.
+  * and/or loose focus.
+* Available functions are:
+  * file comparison of the open file and any of its older versions from the history.
+  * incremental diff view.
+* Functions are available via:
+  * the right-click context menu.
+  * the `Local History: ...` commands from the command palette.
+* `Local History` helps you out when you change or delete a file by accident.
+* `Local History` can help you out when your workspace has a catastrophic problem or if you get disk errors that corrupt your workspace files.
+* Each file revision is stored in a separate file (with full path):
+	* inside the `~/.sublime/Local History/` folder of your home directory (`"portable": false`)
+	* inside the `Sublime Text/Data/.sublime/Local History/` folder of your [Sublime Text](https://www.sublimetext.com) installation (`"portable": true`)
 
 ## Installation
-**With the Package Control plugin:** The easiest way to install this plugin is through [Package Control](http://wbond.net/sublime_packages/package_control)
 
-**Without Git:** Download the latest source zip from [github](https://github.com/vishr/local-history/zipball/master) and extract the files to your Sublime Text "Packages" directory, into a new directory named `Local History`.
-
-**With Git:** Clone the repository in your Sublime Text "Packages" directory:
-
-    git clone https://github.com/vishr/local-history.git "Local History"
-
-The "Packages" directory location:
-
-* OS X:
-    `~/Library/Application Support/Sublime Text 2/Packages/`
-* Linux:
-    `~/.Sublime Text 2/Packages/`
-* Windows:
-    `%APPDATA%/Sublime Text 2/Packages/`
+* Via [Package Control](https://www.packagecontrol.io):
+  * [Install Package Control](https://www.packagecontrol.io/installation)
+  * Open the command palette (<kbd>Ctrl</kbd><kbd>⇧ Shift</kbd><kbd>P</kbd>)
+  * Choose `Install Package`
+  * Search for `Local History` and select to install.
+* Clone the repo: `git clone git://github.com/vishr/local-history.git "Local History"` into your [Sublime Text](https://www.sublimetext.com) Packages directory.
+  * via HTTPS: `https://github.com/vishr/local-history.git`
+  * via SSH: `git@github.com:vishr/local-history.git`
+* Download an archive file below, unpack it and then re-zip the contents of the `Local History` subdirectory. Rename `Local History.zip` to `Local History.sublime-package` and move it to your `Installed Packages` subdirectory of your [Sublime Text](https://www.sublimetext.com) installation:
+  * current snapshot of master
+    * [current snapshot of master as *.zip](https://github.com/vishr/local-history/archive/master.zip)
+    * [current snapshot of master as *.tar.gz](https://github.com/vishr/local-history/archive/master.tar.gz)
 
 ## Usage
-![Open | Compare | Incremental Diff](http://i.imgur.com/jNMrT8Q.png)
 
-![Delete All](http://i.imgur.com/nUlx8.png)
+<img src="./docs/context-menu.png" alt="Context Menu" width="350" height="300">
+
+* Functions are available via:
+  * the right-click context menu.
+  * the `Local History: ...` commands from the command palette.
+
+<img src="./docs/tools-menu.png" alt="Tools Menu" width="400" height="320">
+
+* To permanently delete all history files, choose `Tools > Local History > Delete Local History > Permanently delete all`
 
 ### Settings
+
 ```js
-{
-	  "file_size_limit": 262144, // 256 KB
-	  "history_retention": 30, // in days
-	  "history_on_close": false // store history only on close
-	  "history_path": "<path>" // path to store history, defaults to <HOME>/.sublime/history
-}
+    "history_retention": 0, // number of days to keep files, 0 to disable deletion
+    "format_timestamp": "%Y%m%d%H%M%S", // file_name-XXXXXXXX.file_extension
+    "history_on_close": true,
+    "history_on_focus_lost": false,
+    "history_on_load": true,
+    "portable": true, // save to 'Sublime Text/Data/.sublime/Local History/...' instead of '~/.sublime/Local History/...'
+    "file_size_limit": 4194304 // 4 MB
 ```
